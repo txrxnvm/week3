57adf82ff52e19c03d2d8d3b34d26a5d2d2af1bd
txrxnvme <txrxnvm@gmail.com>
2023-05-08 23:25:58 -0400

    week3 final

---
+++ html/conditionals.js
@@ -0,0 +1,14 @@
+const AGE_LIMIT = 16; 
+let myAge = 15; 
+let hasPermission = false; 
+
+
+if(myAge >= AGE_LIMIT){
+    console.log("YES");
+}else if(hasPermission){
+    console.log("*YES");
+}else{
+    console.log("NO");
+}
+
+console.log("DONE")
---
+++ html/datatype.js
@@ -0,0 +1,13 @@
+
+const firstName = "tara"; 
+let age = 25;
+
+console.log(typeof firstName)
+console.log(typeof age)
+
+const letter = "ABC"
+let ascii_value = letter.charCodeAt(0);
+console.log(ascii_value);
+
+ascii_value = ascii_value + 1;
+console.log(String.fromCharCode(ascii_value))
